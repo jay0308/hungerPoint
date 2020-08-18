9d4e9a74fab4289f45fba29dee2884bd1051a43f
mrityunjay <mrityunjay.karn@paytm.com>
2020-08-18 14:02:31 +0530

    package json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 require('dotenv').config();
-const port = process.env.NODE_ENV === "development" ? 5004 : process.env.PORT || 8080;
+const port = process.env.NODE_ENV === "development" ? 5004 : process.env.PORT || 80;
 const bodyParser = require('body-parser');
 const path = require('path');
 
